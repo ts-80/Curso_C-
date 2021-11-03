970cf9805c3929fc073bc9da59c152166c477bf9
Thiago Siloe <thiagosiloe@gmail.com>
2021-11-03 20:04:47 -0300

    Projeto de estudo

---
+++ Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vetor_2
+{
+    class Product
+    {
+        public string Nome { get; set; }
+        public double Price { get; set; }
+
+         
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetor_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int entrada = int.Parse(Console.ReadLine());
+            Product[] produtos = new Product[entrada];
+
+            for (int i = 0; i < entrada; i++)
+            {
+                string nome = Console.ReadLine();
+                double preco = Convert.ToDouble(Console.ReadLine(),CultureInfo.InvariantCulture);
+                produtos[i] = new Product { Nome = nome, Price = preco };
+    
+            }
+
+            double soma = 0;
+
+            for (int y = 0; y < entrada; y++)
+            {
+                soma += produtos[y].Price;
+            }
+
+            double media = soma / entrada;
+
+            Console.WriteLine("AVERAGE PRICE = " + media.ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+        }
+    }
+}
